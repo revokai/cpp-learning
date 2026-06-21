7b8dbad3f4e6e8183c1aed8e7cb1df9c0dc566df
revokai <shubhpatni6669@gmail.com>
2026-06-21 14:17:32 +0530

    Update area_circle_input.cpp

--- area_circle_input.cpp
+++ area_circle_input.cpp
@@ -3,10 +3,13 @@ using namespace std;
 
 int main()
 {
+    cout << "ENTER RADIUS: ";
+
     float radius;
     cin >> radius;
 
-    float area = 3.141592 * radius * radius;
+    float area = 3.14 * radius * radius;
 
+    cout << "AREA OF CIRCLE IS: ";
     cout << area << endl;
 }
