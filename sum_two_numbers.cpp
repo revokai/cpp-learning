79c9465f9181234c0d3e7e0f5bdb734268164071
revokai <shubhpatni6669@gmail.com>
2026-06-21 19:16:55 +0530

    Create sum_two_numbers.cpp

---
+++ sum_two_numbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a, b;
+
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    float sum = a + b;
+
+    cout << "Sum = " << sum << endl;
+
+    return 0;
+}
