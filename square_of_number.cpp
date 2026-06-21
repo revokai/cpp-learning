f57a5e7afbfabd97d3e5fbf31d79dd15f6bdeb5b
revokai <shubhpatni6669@gmail.com>
2026-06-21 14:47:20 +0530

    Create square_of_number.cpp

---
+++ square_of_number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    float square = number * number;
+
+    cout << "Square of the number is: ";
+    cout << square << endl;
+
+    return 0;
+}
